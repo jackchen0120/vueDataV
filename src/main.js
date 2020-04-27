15918af46aa94108e2df243bddbcdab5ee02f95b
jackchen0120 <278910933@qq.com>
2020-04-28 02:38:57 +0800

    新增金字塔图表数据

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Axios from "axios";
+// import Axios from "axios";
 import vueParticles from 'vue-particles'
 import Vcomp from './components/index'
 import Toast from './components/toast'
-import $ from 'jquery'
+// import $ from 'jquery'
 
 import '@/assets/styles/base.scss'
 import '@/assets/styles/common.scss'
@@ -17,7 +17,7 @@ Vue.use(Vcomp)
 
 Vue.config.productionTip = false
 
-Vue.prototype.$Axios = Axios
+// Vue.prototype.$Axios = Axios
 Vue.prototype.$Toast = Toast
 
 // $(window).resize(() => {
--- vue.config.js
+++ vue.config.js
@@ -19,6 +19,7 @@ module.exports = {
       // 把原本需要写在webpack.config.js中的配置代码 写在这里 会自动合并
       externals: {
            'jquery' : '$',
+           'echarts': 'echarts',
            'axios' : 'axios'
       }
     }
